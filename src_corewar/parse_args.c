9ffbe0abada962fadd1e8e1f00133aa0326678c4
Frenica Provolone <fprovolo@mi-k5.msk.21-school.ru>
2020-12-18 17:19:40 +0300

    чтение файлов в массив cv->players

--- src_corewar/parse_args.c
+++ src_corewar/parse_args.c
@@ -6,7 +6,7 @@
 /*   By: fprovolo <fprovolo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/16 17:09:34 by fprovolo          #+#    #+#             */
-/*   Updated: 2020/12/18 15:59:15 by fprovolo         ###   ########.fr       */
+/*   Updated: 2020/12/18 17:16:54 by fprovolo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,66 +38,82 @@ static void		parse_flags(t_corewar *cv, int *argc, char ***argv)
 // {
 // }
 
-static void	add_player(t_player **players)
-{
-	t_player	*new;
+// static void	add_player(t_player **players)
+// {
+// 	t_player	*new;
 
-	if (!(new = (t_player *)malloc(sizeof(t_player))))
-		terminate("Memory allocation error");
-	new->previous = *players;
-	new->nb_player = new->previous ? new->previous->nb_player + 1 : 1;
-	new->name[PROG_NAME_LENGTH] = '\0';
-	new->comment[COMMENT_LENGTH] = '\0';
-	*players = new;
-	return ;
-}
+// 	if (!(new = (t_player *)malloc(sizeof(t_player))))
+// 		terminate("Memory allocation error");
+// 	new->previous = *players;
+// 	new->nb_player = new->previous ? new->previous->nb_player + 1 : 1;
+// 	new->name[PROG_NAME_LENGTH] = '\0';
+// 	new->comment[COMMENT_LENGTH] = '\0';
+// 	*players = new;
+// 	return ;
+// }
 
-static void		parse_players(t_corewar *cv, int *argc, char ***argv)
+static void		parse_player(t_corewar *cv, char *f_name)
 {
-	t_player	*players;
 	int			fd;
+	t_player	*player;
+	// int			check;
+	// unsigned char	buff[16];
 
-	players = NULL;
-	while (*argc > 0)
-	{
-		if ((fd = open(**argv, O_RDONLY)) == -1)
-			print_usage_exit("File open error");
-		if (!(ft_read32(fd) == COREWAR_EXEC_MAGIC))
-			ft_printf("Wrong magic code!\n");
-		add_player(&players);
-		if (read(fd, players->name, PROG_NAME_LENGTH) != PROG_NAME_LENGTH)
-			print_usage_exit("Incorrect file format. Prog name.");
-		if (ft_read32(fd) != 0)
-			print_usage_exit("Incorrect file format");
-		players->code_len = ft_read32(fd);
-		if (players->code_len < 1 || players->code_len > CHAMP_MAX_SIZE)
-			print_usage_exit("Incorrect code length");
-		if (read(fd, players->comment, COMMENT_LENGTH) != COMMENT_LENGTH)
-			print_usage_exit("Incorrect file format. Comment.");
-		if (ft_read32(fd) != 0)
-			print_usage_exit("Incorrect file format");
-		if (read(fd, players->comment, players->code_len) != players->code_len)
-			print_usage_exit("Incorrect file format. Code length.");
-		cv->nb_players++;
-		(*argc)--;
-		(*argv)++;
+	player = cv->players + cv->nb_players; // ук-ль на очередного плеера. добавить проверку на >4 
+	if ((fd = open(f_name, O_RDONLY)) == -1)
+		print_usage_exit("File open error");
+	if (!(ft_read32(fd) == COREWAR_EXEC_MAGIC))
+		ft_printf("Wrong magic code!\n");
+	if (read(fd, player->name, PROG_NAME_LENGTH) != PROG_NAME_LENGTH)
+		print_usage_exit("Incorrect file format. Prog name.");
+	if (ft_read32(fd) != 0)
+		print_usage_exit("Incorrect file format");
+	player->code_len = ft_read32(fd);
+	if (player->code_len < 1 || player->code_len > CHAMP_MAX_SIZE)
+		print_usage_exit("Incorrect code length");
+	if (read(fd, player->comment, COMMENT_LENGTH) != COMMENT_LENGTH)
+		print_usage_exit("Incorrect file format. Comment.");
+	if (ft_read32(fd) != 0)
+		print_usage_exit("Incorrect file format");
+	if (read(fd, player->comment, player->code_len) != player->code_len)
+		print_usage_exit("Incorrect file format. Code length.");
+	player->nb_player = cv->nb_players + 1;
+	cv->nb_players++;
+	// // Контрольное чтение в конце файла
+	// check = read(fd, buff, 16);
+	// ft_printf("read AFTER: %d\n\n", check);
 
-		ft_printf("\nFile: \t%s\n", **argv);
-		ft_printf("Player \t#%d\n", players->nb_player);
-		ft_printf("Name: \t\"%s\"\n", players->name);
-		ft_printf("Code_len: \t\"%d\"\n", players->code_len);
-		ft_printf("Code: \t\"%s\"\n", players->code);
-	}
 	return ;
 }
 
 
 void			parse_args(t_corewar *cv, int argc, char **argv)
 {
+	int			i;
+
 	argc--;
 	argv++;
 	parse_flags(cv, &argc, &argv);
 	if (argc < 1)
 		print_usage_exit("There are no Players in command line");
-	parse_players(cv, &argc, &argv);
+	while (argc > 0)
+	{
+		// parse_num_flag(cv, &argc, &argv);
+		parse_player(cv, *argv);
+		argc--;
+		argv++;
+	}
+
+	ft_printf("Players: %d\n", cv->nb_players);
+	i = 0;
+	while (i < cv->nb_players)
+	{
+		ft_printf("\nPlayer \t#%d\n", cv->players[i].nb_player);
+		ft_printf("Name: \t\"%s\"\n", cv->players[i].name);
+		ft_printf("Comm.: \t\"%s\"\n", cv->players[i].comment);
+		ft_printf("Length: \t%d\n", cv->players[i].code_len);
+		ft_printf("Code: \t\"%s\"\n", cv->players[i].code);
+		i++;
+	}
+	
 }	
